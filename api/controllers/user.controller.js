c43e17d7716fe3dcfe3601712aea6cd032867703
Chandan Bag <cbag67612@gmail.com>
2024-05-03 18:01:56 +0530

    create sign in api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -3,6 +3,7 @@ import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 
 
@@ -46,3 +47,42 @@ export const registerUser = asyncHandler(async (req, res) => {
     .status(201)
     .json( new ApiResponse(201, 'User created successfully', newUser) )
 }) 
+
+
+export const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    if ([ email, password ].some((field) => field?.trim() === '')) {
+        throw new ApiError(401, 'All fields are required')
+    }
+
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new ApiError(401, 'Invalid email')
+    }
+
+    const isPasswordValid = bcryptjs.compareSync(password, user.password)
+
+    if (!isPasswordValid) {
+        throw new ApiError(401, 'Invali dpassword')
+    }
+
+    const token = jwt.sign(
+        { id: user._id },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: '1d' }
+    )
+
+    const loggedInUser = await User.findById(user._id).select('-password')
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .cookie('access_token', token, options)
+    .json( new ApiResponse(200, 'User logged in successfully', loggedInUser) )
+})
