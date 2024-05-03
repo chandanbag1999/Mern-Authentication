2c77a438384bd15b31a2a589aec255b3a2adf971
Chandan Bag <cbag67612@gmail.com>
2024-05-03 16:42:01 +0530

    add a middleware to handle error

--- api/app.js
+++ api/app.js
@@ -14,4 +14,17 @@ import userRoute from './routes/user.route.js'
 // route decleration
 app.use('/api/v1/users', userRoute)
 
+
+
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+      success: false,
+      message,
+      statusCode,
+    });
+  });
+
 export default app
