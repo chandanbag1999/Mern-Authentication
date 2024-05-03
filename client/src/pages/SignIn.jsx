908665580af890d6f10cc3f0947699afd6065973
Chandan Bag <cbag67612@gmail.com>
2024-05-03 18:52:15 +0530

    complete the sign in ui and functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,86 @@
-import React from 'react'
+import { Alert, Button, Spinner, TextInput } from 'flowbite-react'
+import React, { useState } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
 
 export default function SignIn() {
+  const [formData, setFormData] = useState({})
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.id]: e.target.value})
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+      const response = await fetch('/api/v1/users/sign-in', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json();
+      setLoading(false);
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+      navigate('/');
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  }
   return (
-    <div>SignIn</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-2'>
+      <TextInput 
+          type='email'
+          placeholder='Email'
+          id='email'
+          className=' p-3 rounded-lg'
+          onChange={handleChange}
+        />
+        <TextInput 
+          type='password'
+          placeholder='Password'
+          id='password'
+          className=' p-3 rounded-lg'
+          onChange={handleChange}
+        />
+        <Button
+          gradientDuoTone='purpleToPink'
+          type='submit'
+          disabled={loading}
+        >
+          { loading ? (
+            <>
+              <Spinner size='sm' />
+              <span className='pl-2'>Loading...</span>
+            </>
+          ) : (
+            'Sign In'
+          )}
+        </Button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Dont Have an account?</p>
+        <Link to='/sign-up'>
+          <span className='text-blue-500'>Sign up</span>
+        </Link>
+      </div>
+      { error && (
+        <Alert className='mt-5' color='failure'>
+          {'Something went wrong!'}
+        </Alert>
+      )}
+    </div>
   )
 }
