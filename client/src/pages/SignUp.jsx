87739fe9f0fa5574f67944a307ba7bc9d823ec71
Chandan Bag <cbag67612@gmail.com>
2024-05-03 17:44:18 +0530

    add proxy in frontend and complete sign up functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,33 +1,78 @@
-import { Button, TextInput } from 'flowbite-react';
+import { Alert, Button, Spinner, TextInput } from 'flowbite-react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.id]: e.target.value})
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+      const response = await fetch('/api/v1/users/sign-up', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json();
+      setLoading(false);
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-      <form className='flex flex-col gap-2'>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-2'>
         <TextInput 
           type='text'
           placeholder='Username'
           id='username'
           className=' p-3 rounded-lg'
+          onChange={handleChange}
         />
         <TextInput 
           type='email'
           placeholder='Email'
           id='email'
           className=' p-3 rounded-lg'
+          onChange={handleChange}
         />
         <TextInput 
           type='password'
           placeholder='Password'
           id='password'
           className=' p-3 rounded-lg'
+          onChange={handleChange}
         />
         <Button
           gradientDuoTone='purpleToPink'
+          type='submit'
+          disabled={loading}
         >
-          Sign Up
+          { loading ? (
+            <>
+              <Spinner size='sm' />
+              <span className='pl-2'>Loading...</span>
+            </>
+          ) : (
+            'Sign Up'
+          )}
         </Button>
       </form>
       <div className='flex gap-2 mt-5'>
@@ -36,6 +81,11 @@ export default function SignUp() {
           <span className='text-blue-500'>Sign in</span>
         </Link>
       </div>
+      { error && (
+        <Alert className='mt-5' color='failure'>
+          {'Something went wrong!'}
+        </Alert>
+      )}
     </div>
   );
 }
