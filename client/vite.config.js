9542ac09f97132c42ceefd8392e2d61463d828bd
Chandan Bag <cbag67612@gmail.com>
2024-05-04 15:23:51 +0530

    changed target url

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://mern-authentication-y1jb.onrender.com/',
         secure: false,
       },
     },
